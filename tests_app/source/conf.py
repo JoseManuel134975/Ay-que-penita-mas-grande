e03001a99bca6ce4831ce3b01a22283bfb10bc10
JoseManuel134975 <josemanuel3606@gmail.com>
2025-03-30 21:31:44 +0200

    conf.py -> linea 11

--- tests_app/source/conf.py
+++ tests_app/source/conf.py
@@ -8,10 +8,11 @@
 
 import os
 import sys
-import django
 
 sys.path.insert(0, os.path.abspath('../..'))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RefugioDigital.settings')
+
+import django
 django.setup()
 
 project = 'RefugioDigital'
